ba64f751c14f9059e885cfc650d304f61a561d52
khushal vekariya <i.khushalvekariya@gmail.com>
2024-11-11 17:25:35 +0530

    change numver formate

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -65,7 +65,7 @@
                                             d="M80 0C44.7 0 16 28.7 16 64V448c0 35.3 28.7 64 64 64H304c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H80zm80 432h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H160c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
                                     </svg>
                                 </div>
-                                <p class="max-w-md text-gray-800 opacity-80 underline"> <a href="tel:+9825145424">+982 514 5424</a>
+                                <p class="max-w-md text-gray-800 opacity-80 underline"> <a href="tel:+9825145424">+91 9825145424</a>
                                 </p>
                             </div>
                         </li>
